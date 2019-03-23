af80bb48171b26222727bd1c5377ea59d5c2cd0e
Shofiqur Rahman <rahman@student.tugraz.at>
2019-03-23 22:09:04 +0100

    added app.html

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,15 +64,6 @@
 </template>
 
 <script>
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
 </script>
 
 <style>
